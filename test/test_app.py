538b2a8de2d6ab2d83c78e33c68103094892604a
Miguel <moralesparejamiguel@gmail.com>
2025-06-29 12:34:31 +0200

    test

--- test/test_app.py
+++ test/test_app.py
@@ -1,7 +1,11 @@
-from app import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from run import app
 
 def test_root():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hola" in response.data 
+    assert b"Hola" in response.data
