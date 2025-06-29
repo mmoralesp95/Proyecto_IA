8194e037b2c34a14ccfa0a472846a6eb821de598
Miguel <moralesparejamiguel@gmail.com>
2025-06-29 12:37:49 +0200

    mock test

--- test/test_app.py
+++ test/test_app.py
@@ -1,8 +1,11 @@
 import sys
 import os
+from unittest.mock import patch
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from run import app
+with patch("app.routes.routes.AzureOpenAI"):
+    from run import app
 
 def test_root():
     tester = app.test_client()
